245fe0d36df8f2d60ed5e3e6ab1707d5cf494a20
Ryan Bottema <ryanbottema@gmail.com>
2021-04-05 14:33:45 -0400

    ...

--- propygate/celery.py
+++ propygate/celery.py
@@ -44,3 +44,23 @@ app.conf.broker_url = BASE_REDIS_URL
 
 # this allows you to schedule items in the Django admin.
 app.conf.beat_scheduler = 'django_celery_beat.schedulers.DatabaseScheduler'
+
+
+from celery.schedules import crontab
+app.conf.beat_schedule = {
+    'check_enviros_every_5min': {
+        'task': 'check_enviros',
+        'schedule': 5 * 60,   # in seconds
+        'args': (1),   # assume enviro id of 1
+    },
+    # 'add-every-5-seconds': {
+    #     'task': 'multiply_two_numbers',
+    #     'schedule': 5.0,
+    #     'args': (16, 16)
+    # },
+    # 'add-every-30-seconds': {
+    #     'task': 'tasks.add',
+    #     'schedule': 30.0,
+    #     'args': (16, 16)
+    # },
+}
